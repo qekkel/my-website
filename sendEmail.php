6a9a5d2b633b76d3780579dfc227c48f5e252de9
qekkel <qekkel.olia@gmail.com>
2025-01-28 15:07:20 +0300

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $firstName = $_POST['first-name'];
+  $lastName = $_POST['last-name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Формирование письма
+  $to = "qekkel.olia@gmail.com";
+  $subject = "New Contact Form Message";
+  $body = "First Name: $firstName\nLast Name: $lastName\nEmail: $email\nMessage: $message";
+  $headers = "From: $email";
+
+  // Отправка письма
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Message sent successfully!";
+  } else {
+    echo "Failed to send message.";
+  }
+}
+?>
