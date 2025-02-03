80768e09231a77c959c0235453b38b4c5d97ec7d
qekkel <qekkel.olia@gmail.com>
2025-02-03 09:18:46 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const grid = document.querySelector('.gallery-container');
     const masonry = new Masonry(grid, {
         itemSelector: '.gallery-item',
-          columnWidth: 300,  // Укажите фиксированную ширину для колонок
+          columnWidth: '.gallery-item',  // Укажите фиксированную ширину для колонок
           percentPosition: true,
           gutter: 20  // Вы можете также уменьшить gutter, если нужно
     });
   
     imagesLoaded(grid, function() {
       masonry.layout();
-    });
+    // Перерасчет layout при изменении размера окна
+    window.addEventListener('resize', function () {
+      masonry.layout();
   });
+});
