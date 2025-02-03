0d1f296e254b0949cf2c718d65d29fa9b7b4b4bf
qekkel <qekkel.olia@gmail.com>
2025-02-03 17:38:57 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const grid = document.querySelector('.gallery-container');
-    const masonry = new Masonry(grid, {
-        itemSelector: '.gallery-item',
-          columnWidth: '.gallery-item',  // Укажите фиксированную ширину для колонок
-          percentPosition: true,
-          gutter: 20  // Вы можете также уменьшить gutter, если нужно
-    });
-  
-    imagesLoaded(grid, function() {
-      masonry.layout();
-    // Перерасчет layout при изменении размера окна
-    window.addEventListener('resize', function () {
-      masonry.layout();
+  // Получаем контейнер для галереи
+  const grid = document.querySelector('.gallery-container');
+
+  if (!grid) {
+    console.error('Галерея не найдена!');
+    return;
+  }
+
+  // Инициализируем Masonry
+  const masonry = new Masonry(grid, {
+    itemSelector: '.gallery-item',
+    columnWidth: '.gallery-item', // Укажите фиксированную ширину для колонок
+    percentPosition: true,
+    gutter: 20  // Вы можете также уменьшить gutter, если нужно
   });
+
+  // После того как изображения загружены, вызываем layout для Masonry
+  imagesLoaded(grid, function() {
+    masonry.layout();
+  });
+
+  // Перерасчет layout при изменении размера окна
+  window.addEventListener('resize', function () {
+    masonry.layout();
+  });
+
 });
